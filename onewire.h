c3611ea0aff41adf9f7c5c4d1a1a7b6e569a9b9a
arty1223 <{ID}+{username}@users.noreply.github.com>
2025-07-16 10:35:51 +0300

    Комментарии

--- onewire.h
+++ onewire.h
@@ -13,11 +13,13 @@
 #include "app.h"
 
 
-void initOneWire(void); // инициализация OneWire на портах PB5 PB6. PB5 и PB6 должны быть соединены между собой.
-uint8_t OneWire_Reset(void);
-uint8_t OneWire_ReadBit(void);
-uint8_t OneWire_ReadByte(void);
-void OneWire_WriteBit(uint8_t);
-void OneWire_WriteByte(uint8_t);
+void initOneWire(void); // Инициализация OneWire на портах PB5 PB6. PB5 и PB6 должны быть соединены между собой.
+uint8_t OneWire_Reset(void); // Сброс шины OneWire (Используется для начала работы с устройствами на шине)
+
+// Функции для работы с OneWire
+uint8_t OneWire_ReadBit(void); // Чтение бита из шины OneWire
+uint8_t OneWire_ReadByte(void); // Чтение байта из шины OneWire
+void OneWire_WriteBit(uint8_t); // Запись бита в шину OneWire
+void OneWire_WriteByte(uint8_t); // Запись байта в шину OneWire
 
 #endif /* INC_ONEWIRE_H_ */
